05a0599d0d6dbb52e86d38a5bd87f31294629a0f
faticc <komeristyj07@gmail.com>
2024-06-01 01:41:54 +0300

    NewDay

--- js/date.js
+++
@@ -1,4 +0,0 @@
-var date = new Date();
-const options = { year: 'numeric', month: 'long', day: 'numeric' };
-const dateString = date.toLocaleString('ru-RU', options);
-document.body.innerHTML += '<h3><p class="text-center">' + dateString + '</p></h3>';
